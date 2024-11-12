3b488e59f58a784eef45293eb24282bf21052397
Zawa <155267500+rqms40@users.noreply.github.com>
2024-11-12 22:33:30 +0800

    refactor: improve structure

--- blogsposts.go
+++ blogsposts.go
@@ -1,31 +1,20 @@
 package blogposts
 
 import (
-	"io"
 	"io/fs"
-	"strings"
 )
-
-type Post struct {
-	Title string
-}
 
 func PostsFromFS(fileSystem fs.FS) []Post {
 	dir, _ := fs.ReadDir(fileSystem, ".")
 	var posts []Post
 	for _, f := range dir {
 
-		posts = append(posts, makePostFromFile(fileSystem, f))
+		posts = append(posts, makePostFromFile(fileSystem, f.Name()))
 	}
 	return posts
 }
 
-func makePostFromFile(fileSystem fs.FS, f fs.DirEntry) Post {
-	blogFile, _ := fileSystem.Open(f.Name())
-	fileContents, _ := io.ReadAll(blogFile)
-
-	title := strings.TrimPrefix(string(fileContents), "Title: ")
-	return Post{
-		Title: title,
-	}
+func makePostFromFile(fileSystem fs.FS, fileName string) Post {
+	blogFile, _ := fileSystem.Open(fileName)
+	return newPost(blogFile)
 }
---
+++ post.go
@@ -0,0 +1,19 @@
+package blogposts
+
+import (
+	"io"
+	"strings"
+)
+
+type Post struct {
+	Title string
+}
+
+func newPost(blogFile io.Reader) Post {
+	fileContents, _ := io.ReadAll(blogFile)
+
+	title := strings.TrimPrefix(string(fileContents), "Title: ")
+	return Post{
+		Title: title,
+	}
+}
